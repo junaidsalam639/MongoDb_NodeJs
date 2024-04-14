93cd61fc9ea6d6858726d0aa2e25c6184436c3c3
junaidhnhtechsolutions <junaid@hnhtechsolutions.com>
2024-04-14 21:41:25 +0500

    'nodejs'

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsConfig = {
-    origin : "*",
-    credentials : true,
-    methods : ["GET", "POST", "PUT", "DELETE"],
-}
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 // const User = require("./Routes/UserRoutes");
 const Users = require("./Schema/UserSchema");
 const Port = 9000;
@@ -16,13 +16,19 @@ app.use(cors(corsConfig));
 app.use(express.json());
 // app.use("/users", User);
 
-
-mongoose.connect("mongodb+srv://junaidsalam639:5qkb1exmZCJFIO0P@cluster0.g91jxfi.mongodb.net/").then(() => {
+mongoose
+  .connect(
+    "mongodb+srv://junaidsalam639:5qkb1exmZCJFIO0P@cluster0.g91jxfi.mongodb.net/"
+  )
+  .then(() => {
     console.log("DB Connected");
-}).catch((err) => {
+    app.listen(Port, () => {
+      console.log(`Server is running on port ${Port}`);
+    });
+  })
+  .catch((err) => {
     console.log(err);
-});
-
+  });
 
 // app.get("/", (req, res) => {
 //     res.send({
@@ -32,16 +38,10 @@ mongoose.connect("mongodb+srv://junaidsalam639:5qkb1exmZCJFIO0P@cluster0.g91jxfi
 // })
 
 app.get("/", async (req, res) => {
-    const user = await Users.find();
-    res.send({
-        status : "OK",
-        message : "User Get Success",
-        data : user,
-    });
-})
-
-app.listen(Port, () => {
-  console.log(`Server is running on port ${Port}`);
+  const user = await Users.find();
+  res.send({
+    status: "OK",
+    message: "User Get Success",
+    data: user,
+  });
 });
-
-
