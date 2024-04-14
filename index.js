16de86813c81838ffc61ddb1cd6bf2742f894c5d
junaidhnhtechsolutions <junaid@hnhtechsolutions.com>
2024-04-14 21:43:11 +0500

    'nodejs'

--- index.js
+++ index.js
@@ -3,45 +3,35 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-// const User = require("./Routes/UserRoutes");
-const Users = require("./Schema/UserSchema");
+    origin : "*",
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE"],
+}
+const User = require("./Routes/UserRoutes");
 const Port = 9000;
 
 app.options("*", cors(corsConfig));
 app.use(cors(corsConfig));
 app.use(express.json());
-// app.use("/users", User);
+app.use("/users", User);
 
-mongoose
-  .connect(
-    "mongodb+srv://junaidsalam639:5qkb1exmZCJFIO0P@cluster0.g91jxfi.mongodb.net/"
-  )
-  .then(() => {
+
+mongoose.connect("mongodb+srv://junaidsalam639:5qkb1exmZCJFIO0P@cluster0.g91jxfi.mongodb.net/").then(() => {
     console.log("DB Connected");
-    app.listen(Port, () => {
-      console.log(`Server is running on port ${Port}`);
-    });
-  })
-  .catch((err) => {
+}).catch((err) => {
     console.log(err);
-  });
-
-// app.get("/", (req, res) => {
-//     res.send({
-//         status : "OK",
-//         message : "Hello World"
-//     });
-// })
-
-app.get("/", async (req, res) => {
-  const user = await Users.find();
-  res.send({
-    status: "OK",
-    message: "User Get Success",
-    data: user,
-  });
 });
+
+
+app.get("/", (req, res) => {
+    res.send({
+        status : "OK",
+        message : "Hello World"
+    });
+})
+
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
+
+
