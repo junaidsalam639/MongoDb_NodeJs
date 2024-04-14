00be92a7a08a294dd553bc85f0ade0f1742f1f52
junaidhnhtechsolutions <junaid@hnhtechsolutions.com>
2024-04-14 21:35:36 +0500

    'nodejs'

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
+    origin : "*",
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE"],
+}
 const User = require("./Routes/UserRoutes");
 const Port = 9000;
 
@@ -15,19 +15,23 @@ app.use(cors(corsConfig));
 app.use(express.json());
 app.use("/users", User);
 
+
 mongoose.connect("mongodb+srv://junaidsalam639:5qkb1exmZCJFIO0P@cluster0.g91jxfi.mongodb.net/").then(() => {
     console.log("DB Connected");
-    app.listen(Port, () => {
-      console.log(`Server is running on port ${Port}`);
-    });
-  })
-  .catch((err) => {
+}).catch((err) => {
     console.log(err);
-  });
+});
+
 
 app.get("/", (req, res) => {
-  res.send({
-    status: "OK",
-    message: "Hello World",
-  });
+    res.send({
+        status : "OK",
+        message : "Hello World"
+    });
+})
+
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
 });
+
+
